08d79688b9b428e7db146ceba2738a121c07d8f8
kaora0 <Ka0rin@yandex.ru>
2024-04-16 19:53:17 +0300

    public

---
+++ LabAAA_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LabAAA_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+            .AddTransient<IMessageService, GreetingService>()
+            //.AddTransient<IMessageService, AlternateMessageService>()
+            .BuildServiceProvider();
+
+            var messageService = serviceProvider.GetRequiredService<IMessageService>();
+            messageService.PrintMessage();
+        }
+    }
+    public interface IMessageService
+    {
+        void PrintMessage();
+    }
+
+    public class GreetingService : IMessageService
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("Здравия желаю!");
+        }
+    }
+
+    public class AlternateMessageService : IMessageService
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("Как жизнь, товарищ?");
+        }
+    }
+}
